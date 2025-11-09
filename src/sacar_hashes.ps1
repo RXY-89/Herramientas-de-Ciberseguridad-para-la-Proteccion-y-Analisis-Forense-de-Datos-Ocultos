0f95d1be9acae6415ce6fd420c804d6dd7f98afc
Guy2054 <simon.robledos@uanl.edu.mx>
2025-11-09 17:05:21 -0600

    Modificaciones el script de PowerShell
    Modifique detallitos que me molestaban al intentar usarlo desde Python

--- src/sacar_hashes.ps1
+++ src/sacar_hashes.ps1
@@ -1,36 +1,33 @@
-﻿param(
-    [string]$ruta,
-    [bool]$guardar=$false,
-    [string]$carpeta
+param(
+    [Parameter(Mandatory=$true)][string]$ruta,
+    [ValidateSet("True","False")]$guardar="False",
+    [string]$archivo
 )
 
-if ( -not $ruta){
-    Write-Error "No se proporcionó el parametro 'ruta'" -Category InvalidArgument
-    exit 1
-}elseif ( -not (Test-Path $ruta)){
-    Write-Error "El parametro 'ruta' no es una ruta valida" -Category ObjectNotFound
+
+if ( -not (Test-Path $ruta)){
+    Write-Error "El parametro 'ruta' no es una ruta valida" -Category InvalidArgument
     exit 1
 }
-if ($guardar){
-    if ( -not $carpeta){
-        Write-Error "No se proporcionó el parametro 'carpeta'" -Category InvalidArgument
+if ($guardar -eq "True"){
+    if ( -not $archivo){
+        Write-Error "No se proporcionó el parametro 'archivo'" -Category ObjectNotFound
         exit 1
-    } elseif ( -not (Test-Path $carpeta -PathType Container)){
-        Write-Error "El parametro 'carpeta' no es un directorio válido" -Category ObjectNotFound
+    } elseif ( -not (Test-Path (Split-Path $archivo))){
+        Write-Error "El parametro 'archivo' no tiene su carpeta padre" -Category InvalidArgument
         exit 1
     }
 }
 
 if (Test-Path $ruta -PathType Leaf) {
     $hash=Get-FileHash -Path $ruta
-    if ($guardar){
-        $archivo="$carpeta\lista_hashes.json"
+    if ($guardar -eq "True"){
         if ( -not (Test-Path "$archivo")) {@{} | ConvertTo-Json | Set-Content -Path "$archivo"}
         $json=Get-Content $archivo | ConvertFrom-Json
         try{$json.$ruta="$($hash.Hash)"}
         catch{$json | Add-Member -NotePropertyName "$ruta" -NotePropertyValue "$($hash.Hash)"}
         $json | ConvertTo-Json | Set-Content -Path $archivo
-    }else{
+    } else{
         Write-Output $hash.Hash
     }
 } else{
@@ -39,11 +36,9 @@ if (Test-Path $ruta -PathType Leaf) {
         $hash=Get-FileHash -Path $_.FullName
         $hashes["$($_.name)"]="$($hash.Hash)"
     }
-    if ($guardar){
-        $nombre = Split-Path $ruta -Leaf
-        $hashes | ConvertTo-Json | Set-Content -Path "$carpeta\hashes_$nombre.json"
+    if ($guardar -eq "True"){
+        $hashes | ConvertTo-Json | Set-Content -Path "$archivo"
     }else{
         $hashes | ConvertTo-Json -Compress
     }
-    
 }
